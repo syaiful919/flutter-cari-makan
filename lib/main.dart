7c17cf3a209568f44297ca565340efe1f99beff3
syaiful919 <syaiful919@gmail.com>
2021-01-26 16:22:30 +0700

    init project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cari Makan',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text("blank"),
+        ),
+      ),
+    );
+  }
+}
